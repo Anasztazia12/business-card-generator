62705eb1de32908d581214ec85369a064d3f6089
Anasztazia12 <Anasztazia12@users.noreply.github.com>
2026-06-15 15:27:56 +0100

    add: implement background image picker button and enhance card logo positioning

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// get the elements from the page
+
+// Text inputs
+const nameInput = document.getElementById("name");
+const titleInput = document.getElementById("title");
+const companyInput = document.getElementById("company");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+
